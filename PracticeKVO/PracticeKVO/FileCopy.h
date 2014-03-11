2744c8436b7684d2a7246b6df7ab022a7444271a
el1ven <tangGPA@gmail.com>
2014-03-11 22:09:36 +0800

    for comment

--- PracticeKVO/PracticeKVO/FileCopy.h
+++ PracticeKVO/PracticeKVO/FileCopy.h
@@ -10,13 +10,13 @@
 
 @interface FileCopy : NSObject
 {   @private
-    NSString *srcPath;
-    NSString *targetPath;
+    NSString *srcPath;//the source path for the file while has existed
+    NSString *targetPath;// store the target file of path while we will build later
 }
-@property(nonatomic,assign)float fileSize;
-@property(nonatomic,assign)float readSize;
+@property(nonatomic,assign)float fileSize; //the total size of the size which we need to copy
+@property(nonatomic,assign)float readSize; //the size which we are having been copying 
 
--(id)initPath:(NSString *)srcPath targetPath:(NSString*) targetPath;
--(void)startCopy;
+-(id)initPath:(NSString *)srcPath targetPath:(NSString*) targetPath;//init method
+-(void)startCopy;//start to copy, execute method
 
 @end
